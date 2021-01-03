e4b323964a71139f56c265bd139aa50343a1e57a
Giammarco <giammi56@hotmail.com>
2021-01-03 22:28:32 +0100

    rearranging the functions adding some comments

--- functions_giammi.py
+++ functions_giammi.py
@@ -1,26 +1,31 @@
+import itertools
 from itertools import count
 import numpy as np
 import math
 from numpy.core.defchararray import array
 import pandas as pd
-from scipy.interpolate import griddata
 from matplotlib import cm
 import matplotlib as mpl
 import matplotlib.colors
+import triangulation as tr
+import scipy as sp
+import scipy.ndimage
+from scipy.spatial import Delaunay
+from scipy.interpolate import griddata
+from pyntcloud import PyntCloud, structures
+import plotly
+import plotly.graph_objects as go
+import uproot
+import uproot_methods
 
 
-def matplotlib_to_plotly(cmap, pl_entries):
-    """
-    To be used in cmaptep
-    """
-    h = 1.0/(pl_entries-1)
-    pl_colorscale = []
-
-    for k in range(pl_entries):
-        C = list(map(np.uint8, np.array(cmap(k*h)[:3])*255))
-        pl_colorscale.append([k*h, 'rgb'+str((C[0], C[1], C[2]))])
-
-    return pl_colorscale
+def cosphi_func(key, cosphi):
+    ctheta_nc = float((str(key).split("costheta_")[1]).split("_phi")[0])
+    phi_nc = float((str(key).split("phi_")[1]).split(";")[0])
+    # alternative method with import re
+    #ctheta_n=re.search("costheta_(.*)_phi", str(key)).group(1)
+    cosphi.append((ctheta_nc, phi_nc))
+    return cosphi
 
 def customcmaps():
     """
@@ -58,57 +63,110 @@ def customcmaps():
 
     return(cmap_temp, cmap_temp_go, Magma_r, Seismic_r)
 
-def sorting_array(inarray, cosphi, phiM, cosM, a):
+def getamesh(x,y,z,d):
+    temp = pd.DataFrame(np.hstack((x[:,None], y[:,None], z[:,None])))
+    temp.columns = ["x", "y", "z"]
+    sph = PyntCloud(temp)
+    x,  y,  z = sph.points["x"], sph.points["y"], sph.points["z"]
+    pts = sph.points[['x', 'y', 'z']]
+    delaun = structures.Delaunay3D(pts)
+
+    delaun.compute()
+    mesh = delaun.get_mesh()
+    tri = mesh[['v1', 'v2', 'v3']].values
+    I, J, K = tri.T
+    return x,y,z,I,J,K
+
+def import_MFPAD(file, loc, MFPAD, cosphi, MFPAD_xy, ctheta, ctheta_c, ctheta_cred, run_MFPAD=0., run_cos=0.):
     """
-    a is the level according to which values have to be sorted: 1 = cos_light, 2 = phi_light
-    Function to sort the input array according to the labels attached from LMF2ROOT. The lables are stored in cosphi during the loaing.
-    To load sorting the 72 items according to cos(theta) use level = 1, according to phi level = 2. The first level is a tring, thereofere
-    is not possible to sort according to it.
-    NOTE: for experimental data the default order is according to phi, vice versa for the theoretical MFPADs.
+    Loads the MFPADs and the cos(theta) from the .root files.
+    6 inputs + a parameter
+    NOTE: MFPAD_xy and ctheta_c have originally +1 dimensions compare to the z values.
+    For the sake of iminiut, cos(theta) is centered on the middle of the bins.
     """
-    outarray=[]
-    cosn=np.array([col[0] for col in cosphi]); #list
-    phin=np.array([col[1] for col in cosphi]); #list
-    if inarray.ndim>2:
-        data = inarray.reshape(72, inarray.shape[1]*inarray.shape[2]).T
-        df = pd.DataFrame(
-            data=data,
-            index=pd.MultiIndex.from_product([phiM, cosM],names=["phi","cos(theta)"]),
-            columns=['item {}'.format(i) for i in range(72)])
+    for key, value in file[loc].items():
+        filename=loc+"/"+str(key).split(";")[0].replace("b'","")
+        if "mfpad3d_engate_costheta" in filename.lower():
+            cosphi=cosphi_func(key,cosphi)
+            #just .numpy for uproot3
+            temp=np.array(file[filename].to_numpy())
+            MFPAD.append(temp[0]) # it is a list!
+            if run_MFPAD == 0.:
+                #structure for uproot3
+                # MFPAD_xy.append((temp[1][0][0] , temp[1][0][1])) # phi cos(theta) from 2D
+                #structure for uproot4
+                MFPAD_xy.append((temp[1], temp[2])) # phi cos(theta) from 2D
+                run_MFPAD=1.
+        elif "cos(theta)" in filename.lower():
+            #just .numpy for uproot3
+            temp=np.array(file[filename].to_numpy())
+            ctheta.append(temp[0]) # it is a list!
+            if run_cos == 0.:
+                ctheta_c.append(temp[1])
+                ctheta_cred.append(np.array((ctheta_c[0][1:] + ctheta_c[0][:-1])/2)) #! reduced of 1 dimension
+                run_cos=1.
+    return MFPAD, cosphi, MFPAD_xy, ctheta, ctheta_c, ctheta_cred
 
+def mag(vector):
+    """
+    Return the magnitude of a vector (array) as the square root of the sum of the squares of its components.
+    """
+    return math.sqrt(sum(pow(element, 2) for element in vector))
+
+def matplotlib_to_plotly(cmap, pl_entries):
+    """
+    To be used in cmaptep
+    """
+    h = 1.0/(pl_entries-1)
+    pl_colorscale = []
+
+    for k in range(pl_entries):
+        C = list(map(np.uint8, np.array(cmap(k*h)[:3])*255))
+        pl_colorscale.append([k*h, 'rgb'+str((C[0], C[1], C[2]))])
+
+    return pl_colorscale
+
+def makeamesh (x,y,z,d):
+    points2d_trace=go.Scatter(x=x, y=y, mode='markers', marker_color='red', marker_size=6)
+    point_trace=go.Scatter(x=x, y=y,
+                         mode='markers',
+                         name='points',
+                         marker_color='red',
+                         marker_size=6)
+    pts2d=np.array([x,y]).T
+    tri=Delaunay(pts2d)
+    delaunay_tri = tr.triangulation_edges(pts2d, tri.simplices, linewidth=1)
+
+    i, j, k = tri.simplices.T
+    my_mesh3d = go.Mesh3d(
+                        x = x,
+                        y = y,
+                        z = z,
+                        i=i, j=j, k=k,
+                        colorscale='deep_r',
+                        colorbar_thickness=25,
+                        intensity=d,
+                        flatshading=True)
+
+    points3d=np.array([x,y,z]).T
+    delaun_tri3d=tr.triangulation_edges(points3d, tri.simplices)
+    return delaunay_tri, point_trace, my_mesh3d, delaun_tri3d
+
+def normalise_matrix(a,normtype=0):
+    """
+    It normalises a [n,m] matrix.
+    It is possibel to select the normalization: type=0,type=1.
+    """
+    if normtype==0:
+        row_sums = np.sum(a,axis=1)
+        new_matrix = a / row_sums[:, np.newaxis]
+    elif normtype==1:
+        row_sums = np.linalg.norm(a,axis=1)
+        new_matrix = a / row_sums[:, np.newaxis]
     else:
-        cosbin = inarray.shape[1]
-        data = inarray.T
-        df = pd.DataFrame(
-            data=data,
-            index=np.linspace(-1,1,inarray.shape[1]),
-            columns=['item {}'.format(i) for i in range(72)])
-
-    df1=df.T
-    df1["cos_light"]=cosn
-    df1["phi_light"]=phin
-    df1.set_index("cos_light", append=True, inplace=True) #level=1
-    df1.set_index("phi_light", append=True, inplace=True) #level=2
-
-    if inarray.ndim>2:
-        dfnew=df1
-    else:
-        #here it transforms the DataBAse into a series in order to sort correctly the cos(theta) column too
-        dfnew=df1.stack()
-
-    #level=2 is the standard for experiment, level=1 for theory
-    #kind="mergesort" the concept of stable sorting algorithm
-    dfnew.sort_index(level=a, inplace=True, kind="mergesort")
-    dfnew.reset_index(level=1, drop=True, inplace=True) # removes cos_light
-    dfnew.reset_index(level=1, drop=True, inplace=True) # removes phi
-
-    if inarray.ndim>2:
-        #fast approach with numpy with transpose to rearrange the order of the axes:
-        outarray=(dfnew.T).values.reshape(inarray.shape[1],inarray.shape[2],72).transpose(2, 0, 1)
-    else:
-        outarray=dfnew.values.reshape(72, inarray.shape[1])
-
-    return np.array(outarray)
+        print("Failed to normalise!")
+        new_matrix=a
+    return new_matrix
 
 def overlaygraph(fig):
     """
@@ -138,46 +196,45 @@ def overlaygraph(fig):
 
 def remap(b,lim1_low,lim1_high,lim2_low,lim2_high):
     """
-    Remaps the np array b to the new interval [lim_low,lim_high]
+    Remaps the np.array b of tuples [(col1,col2)] to the new interval [lim_low,lim_high] for both columns.
+    It rounds the numbers to the third decimals.
     """
     col1=np.array([col[0] for col in b])
     col2=np.array([col[1] for col in b])
     col1 = lim1_low + np.divide(lim1_high-lim1_low , np.amax(col1)-np.amin(col1)) * (col1-np.amin(col1))
     col2 = lim2_low + np.divide(lim2_high-lim2_low , np.amax(col2)-np.amin(col2)) * (col2-np.amin(col2))
     b=zip(col1,col2)
-    # b=np.around(b,3)
+    b=np.around(b,3)
     return b
 
-def mag(vector):
-    return math.sqrt(sum(pow(element, 2) for element in vector))
-
-def rot2d_MFPAD(MFPAD,theta,phi,cosphi_adj,phiMM,cosMM,method="linear"):
+def rot2d_MFPAD(MFPAD,ctheta,phi,cosphi_adj,phiMM,cosMM,method="linear"):
     """
     It computes a clockwise rotation in spherical coordinates of the MFPAD. It interpolated the roated MFPAD on a linear phiMM cosMM grid.
     INPUT: theta [DEG] (20000,), phi[DEG] (20000,), cosphi_adj = ([adm],[DEG]) (72,2)
     OUTPUT: MFPAD, rotated cos(theta) [adm] and phi [DEG]
     NOTE: bondaries conditions for theta and phi
     """
-    MFPAD_rot=[];theta_rot=[];phi_rot=[];
+    MFPAD_rot=[];ctheta_rot=[];phi_rot=[];
     for angle in cosphi_adj:
-        for th,ph in zip(theta.reshape(-1),phi.reshape(-1)):
-            testt=th+np.arccos(angle[0])*180./np.pi
-            if testt<0.:
-                theta_rot.append(testt+180.)
-            elif testt>=180.:
-                theta_rot.append(testt-180.)
+        for cth,ph in zip(ctheta.reshape(-1),phi.reshape(-1)):
+            tempct=cth+angle[0]
+            if tempct<-1.:
+                ctheta_rot.append(tempct+2.)
+            elif tempct>=1.:
+                ctheta_rot.append(tempct-2.)
             else:
-                theta_rot.append(testt)
+                ctheta_rot.append(tempct)
 
-            testp=ph+angle[1]
-            if testp<=-180.:
-                phi_rot.append(testp+360.)
-            elif testp>180.:
-                phi_rot.append(testp-360.)
+            tempp=ph+angle[1]
+            if tempp<=-180.:
+                phi_rot.append(tempp+360.)
+            elif tempp>180.:
+                phi_rot.append(tempp-360.)
             else:
-                phi_rot.append(testp)
+                phi_rot.append(tempp)
 
-    ctheta_rot=np.cos(np.array(theta_rot)*np.pi/180.).reshape(72,200,100)
+    # ctheta_rot=np.cos(np.array(theta_rot)*np.pi/180.).reshape(72,200,100)
+    ctheta_rot=np.array(ctheta_rot).reshape(72,200,100)
     phi_rot=np.array(phi_rot).reshape(72,200,100)
 
     for counter,el in enumerate(MFPAD):
@@ -233,7 +290,9 @@ def rot3d_MFPAD(MFPAD,theta_rad,phi_rad,cosphi_adj,phiMM,cosMM,method="linear"):
 
         xyzm = np.stack((x, y, z))
 
-        x_LF, y_LF, z_LF = np.einsum('ik, kj -> ij', rot3d(np.arccos(angle[0]),angle[1]*np.pi/180.,0), xyzm)
+        # x_LF, y_LF, z_LF = np.einsum('ik, kj -> ij', rot3d(np.arccos(angle[0]),angle[1]*np.pi/180.,0), xyzm)
+        #inversion of beta and alpha:JUST A TEST
+        x_LF, y_LF, z_LF = np.einsum('ik, kj -> ij', rot3d(angle[1]*np.pi/180.,np.arccos(angle[0]),0), xyzm)
         mag_LF = np.sqrt(x_LF**2+y_LF**2+z_LF**2)
 
         #cos domain  0 < θ ≤ π
@@ -248,56 +307,6 @@ def rot3d_MFPAD(MFPAD,theta_rad,phi_rad,cosphi_adj,phiMM,cosMM,method="linear"):
 
     return np.array(r).reshape(72,200,100), np.array(ctheta).reshape(72,200,100), np.array(phi).reshape(72,200,100)
 
-import triangulation as tr
-from scipy.spatial import Delaunay
-from pyntcloud import PyntCloud, structures
-
-def getamesh(x,y,z,d):
-    temp = pd.DataFrame(np.hstack((x[:,None], y[:,None], z[:,None])))
-    temp.columns = ["x", "y", "z"]
-    sph = PyntCloud(temp)
-    x,  y,  z = sph.points["x"], sph.points["y"], sph.points["z"]
-    pts = sph.points[['x', 'y', 'z']]
-    delaun = structures.Delaunay3D(pts)
-
-    delaun.compute()
-    mesh = delaun.get_mesh()
-    tri = mesh[['v1', 'v2', 'v3']].values
-    I, J, K = tri.T
-    return x,y,z,I,J,K
-
-import plotly
-import plotly.graph_objects as go
-
-def makeamesh (x,y,z,d):
-    points2d_trace=go.Scatter(x=x, y=y, mode='markers', marker_color='red', marker_size=6)
-    point_trace=go.Scatter(x=x, y=y,
-                         mode='markers',
-                         name='points',
-                         marker_color='red',
-                         marker_size=6)
-    pts2d=np.array([x,y]).T
-    tri=Delaunay(pts2d)
-    delaunay_tri = tr.triangulation_edges(pts2d, tri.simplices, linewidth=1)
-
-    i, j, k = tri.simplices.T
-    my_mesh3d = go.Mesh3d(
-                        x = x,
-                        y = y,
-                        z = z,
-                        i=i, j=j, k=k,
-                        colorscale='deep_r',
-                        colorbar_thickness=25,
-                        intensity=d,
-                        flatshading=True)
-
-    points3d=np.array([x,y,z]).T
-    delaun_tri3d=tr.triangulation_edges(points3d, tri.simplices)
-    return delaunay_tri, point_trace, my_mesh3d, delaun_tri3d
-
-import scipy as sp
-import scipy.ndimage
-
 def smoothgauss(MFPAD, sigmax, sigmay):
     """
     Fucntion the smooths with a 2D gaussian function 2D matrixes. For the experiments
@@ -310,8 +319,8 @@ def smoothgauss(MFPAD, sigmax, sigmay):
 
 def shift_func(a, flag=0.):
     """
-    shifts a linearly monotally increasing array of the average of the first two elements, either along columns or rows.
-    It is manly used to plot cosphi[:,0] in b1 maps. cosphi is an array of tuple [(ctheta,phi)].
+    Shifts a linearly monotally increasing array by a constant  equal to the average of the first two elements,
+    either along columns or rows. It is manly used to plot cosphi[:,0] in b1 maps. cosphi is an array of tuple [(ctheta,phi)].
     """
     #if it is a list = 72
     if a.shape[0]>12:
@@ -321,6 +330,9 @@ def shift_func(a, flag=0.):
         # pick the first two elements along the first column
         # and computer the average. This number is the same for all elements of the array
         if c[0][0] == c[0][1]:
+            #this is the case of cosphi
+            if c[0][0] == c[1][0]:
+                c=a.reshape(6,12)
             b=(c[1][0]-c[0][0])/2
             a=np.add(c,b)
         else:
@@ -339,13 +351,17 @@ def shift_func(a, flag=0.):
         a=a.reshape(-1)
     return a
 
-def cosphi_func(key, cosphi):
-    ctheta_nc = float((str(key).split("costheta_")[1]).split("_phi")[0])
-    phi_nc = float((str(key).split("phi_")[1]).split(";")[0])
-    # alternative method with import re
-    #ctheta_n=re.search("costheta_(.*)_phi", str(key)).group(1)
-    cosphi.append((ctheta_nc, phi_nc))
-    return cosphi
+def avg_func(cosphi):
+    """
+    Shifts an array by the average of the each step. It reduces the lenght of the input array by 1.
+    The input has to have a shape (13,7). Output (12,6).
+    """
+    cosu=np.unique([col[0] for col in cosphi])
+    phiu=np.unique([col[1] for col in cosphi])
+    col1=[(a + b) / 2 for a, b in zip(cosu[::], cosu[1::])]
+    col2=[(a + b) / 2 for a, b in zip(phiu[::], phiu[1::])]
+
+    return np.around(np.array(list(itertools.product(col1,col2))),3)
 
 def projection(MFPAD, a):
     """
@@ -361,41 +377,54 @@ def projection(MFPAD, a):
         projected=(MFPAD.sum(axis=a))
     return np.array(projected)
 
-def normalise_matrix(a):
-    row_sums = a.sum(axis=1)
-    new_matrix = a / row_sums[:, np.newaxis]
-    return new_matrix
-
-import uproot
-import uproot_methods
-
-def import_MFPAD(file, loc, MFPAD, cosphi, MFPAD_xy, ctheta, ctheta_c, ctheta_cred, run_MFPAD=0., run_cos=0.):
+def sorting_array(inarray, cosphi, phiM, cosM, a):
     """
-    Loads the MFPADs and the cos(theta) from the .root files.
-    6 inputs + a parameter
-    NOTE: MFPAD_xy and ctheta_c have originally +1 dimensions compare to the z values.
-    For the sake of iminiut, cos(theta) is centered on the middle of the bins.
+    a is the level according to which values have to be sorted: 1 = cos_light, 2 = phi_light
+    Function to sort the input array according to the labels attached from LMF2ROOT. The lables are stored in cosphi during the loaing.
+    To load sorting the 72 items according to cos(theta) use level = 1, according to phi level = 2. The first level is a string, thereofere
+    is not possible to sort according to it.
+    NOTE: for experimental data the default order is according to phi, vice versa for the theoretical MFPADs.
     """
-    for key, value in file[loc].items():
-        filename=loc+"/"+str(key).split(";")[0].replace("b'","")
-        if "mfpad3d_engate_costheta" in filename.lower():
-            cosphi=cosphi_func(key,cosphi)
-            #just .numpy for uproot3
-            temp=np.array(file[filename].to_numpy())
-            MFPAD.append(temp[0]) # it is a list!
-            if run_MFPAD == 0.:
-                #structure for uproot3
-                # MFPAD_xy.append((temp[1][0][0] , temp[1][0][1])) # phi cos(theta) from 2D
-                #structure for uproot4
-                MFPAD_xy.append((temp[1], temp[2])) # phi cos(theta) from 2D
-                run_MFPAD=1.
-        elif "cos(theta)" in filename.lower():
-            #just .numpy for uproot3
-            temp=np.array(file[filename].to_numpy())
-            ctheta.append(temp[0]) # it is a list!
-            if run_cos == 0.:
-                ctheta_c.append(temp[1])
-                ctheta_cred.append(np.array((ctheta_c[0][1:] + ctheta_c[0][:-1])/2)) #! reduced of 1 dimension
-                run_cos=1.
-    return MFPAD, cosphi, MFPAD_xy, ctheta, ctheta_c, ctheta_cred
+    outarray=[]
+    cosn=np.array([col[0] for col in cosphi]); #list
+    phin=np.array([col[1] for col in cosphi]); #list
+    if inarray.ndim>2:
+        data = inarray.reshape(72, inarray.shape[1]*inarray.shape[2]).T
+        df = pd.DataFrame(
+            data=data,
+            index=pd.MultiIndex.from_product([phiM, cosM],names=["phi","cos(theta)"]),
+            columns=['item {}'.format(i) for i in range(72)])
 
+    else:
+        cosbin = inarray.shape[1]
+        data = inarray.T
+        df = pd.DataFrame(
+            data=data,
+            index=np.linspace(-1,1,inarray.shape[1]),
+            columns=['item {}'.format(i) for i in range(72)])
+
+    df1=df.T
+    df1["cos_light"]=cosn
+    df1["phi_light"]=phin
+    df1.set_index("cos_light", append=True, inplace=True) #level=1
+    df1.set_index("phi_light", append=True, inplace=True) #level=2
+
+    if inarray.ndim>2:
+        dfnew=df1
+    else:
+        #here it transforms the DataBAse into a series in order to sort correctly the cos(theta) column too
+        dfnew=df1.stack()
+
+    #level=2 is the standard for experiment, level=1 for theory
+    #kind="mergesort" the concept of stable sorting algorithm
+    dfnew.sort_index(level=a, inplace=True, kind="mergesort")
+    dfnew.reset_index(level=1, drop=True, inplace=True) # removes cos_light
+    dfnew.reset_index(level=1, drop=True, inplace=True) # removes phi
+
+    if inarray.ndim>2:
+        #fast approach with numpy with transpose to rearrange the order of the axes:
+        outarray=(dfnew.T).values.reshape(inarray.shape[1],inarray.shape[2],72).transpose(2, 0, 1)
+    else:
+        outarray=dfnew.values.reshape(72, inarray.shape[1])
+
+    return np.array(outarray)
